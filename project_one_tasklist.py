be98fe148620daad08ea5d720d3e9a7652fb9db9
samplecodesforfun <sample.robertk@gmail.com>
2025-03-10 13:32:22 -0400

    2nd Day
    Changes include a better view task list function that shows the order of each task, also a better add task list function that allows you to add in the task with the order you want it completed in the task list

--- project_one_tasklist.py
+++ project_one_tasklist.py
@@ -1,27 +1,36 @@
 #list that stores all our currently saved tasks
-task_list = []
+task_list = ["wake up","shower","go to work", "dinner", "sleep"]
 
 #method to run the task program on the terminal
 def interface():
-    user_input = input("Would you like to view your tasks, add a new task, or remove an old task?")
-    if(user_input == "view"):
-        print("hi")
-    elif(user_input == "add"):
-        print("hello")
-    elif(user_input == "remove"):
-        print("bonjour")
-    else:
-        print("error")
+    while(True):
+        print("Would you like to view your tasks, add a new task, or remove an old task?")
+        print("Enter exit if your finished.")
+        user_input = input()
+        if(user_input == "view"):
+            viewTaskList()
+        elif(user_input == "add"):
+            addTask()
+        elif(user_input == "remove"):
+            subtractTask()
+        elif(user_input == "exit"):
+            break
+        else:
+            print("user input error")
 
 #method to view the current list
 def viewTaskList():
-    print(task_list)
+    for task in task_list:
+        #takes the index of the task in the list and adds 1 to show its task # and task name and prints it
+        print(int(task_list.index(task)+1), task)
 
 # method to add new tasks to the list
 def addTask():
-    new_task = input("What task would you like to add:")
-    task_list.append(new_task);
-    return new_task
+    task_name = input("What task would you like to add:")
+    #needs conditionals to stop if the number from going to the negatives
+    task_number = int(input("What task # would you like to assign this task: "))
+    task_list.insert(task_number-1,task_name)     
+    
 
 #method to remove old tasks that were completed
 def subtractTask():
@@ -29,4 +38,8 @@ def subtractTask():
     task_list.remove(old_task)
     return old_task
 
+
+#viewTaskList()
+#addTask()
+#subtractTask()
 interface()
