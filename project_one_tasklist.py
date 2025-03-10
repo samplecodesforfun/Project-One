db7253a8e94b010d37109b2a8867928d1fe3d612
samplecodesforfun <sample.robertk@gmail.com>
2025-03-10 16:01:29 -0400

    More Changes
    I added a text file to be able to load and save the task list, I added code to the add task function to also and add to the save file, and added a loadtask list function to load the tasks from the file when reusing the program.

--- project_one_tasklist.py
+++ project_one_tasklist.py
@@ -1,8 +1,9 @@
 #list that stores all our currently saved tasks
-task_list = ["wake up","shower","go to work", "dinner", "sleep"]
+task_list = []
 
 #method to run the task program on the terminal
 def interface():
+    loadTaskList()
     while(True):
         print("Would you like to view your tasks, add a new task, or remove an old task?")
         print("Enter exit if your finished.")
@@ -18,13 +19,25 @@ def interface():
         else:
             print("user input error")
 
+#loads our task list from the previous save on our text file
+def loadTaskList():
+    task_file = open("tasklist.txt", "r")
+    tasks = task_file.readlines()
+    task_file.close()
+    for task in tasks:
+        if task[-1] == "\n":
+            task_list.append(task[:-1])
+        else:
+            task_list.append(task)
+
+
 #method to view the current list
 def viewTaskList():
     for task in task_list:
         #takes the index of the task in the list and adds 1 to show its task # and task name and prints it
         print(int(task_list.index(task)+1), task)
 
-# method to add new tasks to the list
+#method to add new tasks to the list
 def addTask():
     task_name = input("What task would you like to add:")
     task_number = int(input("What task # would you like to assign this task: "))
@@ -32,16 +45,23 @@ def addTask():
     while task_number <= 0:
         print("The task number cannot be 0 or a negative number.");
         task_number = int(input("What task # would you like to assign this task: "))
-    task_list.insert(task_number-1,task_name)     
-    
+    task_list.insert(task_number-1,task_name)
 
+    #adds our new task to the save file, empties the list, and re adds all the tasks in order to the save file
+    task_file = open("tasklist.txt", "w")
+    task_file.write('');
+    for task in task_list:
+        task_file.write(task + "\n")
+    task_file.close()
+
+    
 #method to remove old tasks that were completed
 def subtractTask():
     old_task = input("What task would you like to remove: ")
     task_list.remove(old_task)
     return old_task
 
-
+#loadTaskList()
 #viewTaskList()
 #addTask()
 #subtractTask()
